4b52418f41b865e2c496bb13e86c31a8d145eabb
grant smith <grantsmith1256@gmail.com>
2019-03-25 18:12:53 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,4 +4,8 @@ class CouponsController < ApplicationController
     @coupons = Coupon.all
   end
 
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+  end
+
 end
